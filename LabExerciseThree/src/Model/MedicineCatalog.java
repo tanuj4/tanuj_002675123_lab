2a2633ddc16e2488458f7762612ffc8d82746ad6
kodali tanuj <kodali.t@northeastern.edu>
2023-02-05 16:03:05 -0500

    lab work done

---
+++ LabExerciseThree/src/Model/MedicineCatalog.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tanujkodali
+ */
+public class MedicineCatalog {
+    ArrayList<Medicine> medicineCatalog;
+    
+    public MedicineCatalog(){
+        this.medicineCatalog = new ArrayList<Medicine>();
+    }
+
+    public ArrayList<Medicine> getMedicineCatalog() {
+        return medicineCatalog;
+    }
+
+    public void setMedicineCatalog(ArrayList<Medicine> medicineCatalog) {
+        this.medicineCatalog = medicineCatalog;
+    }
+    
+    public Medicine createMedicine(String name, Double dosage){
+        Medicine medicine=new Medicine();
+        
+        medicine.setDosage(dosage);
+        medicine.setMedicineName(name);
+        
+        this.medicineCatalog.add(medicine);
+        return medicine;
+    }
+    
+    //check if medicine already exists
+    public Boolean checkIfMedicineUnique(String name){
+        for (Medicine m: this.medicineCatalog){
+            if(m.getMedicineName() == name){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //delete a medicine from catalog
+    public void deleteMedicine(String name){
+        //find thhe medicine object in the array list with this name
+        
+        for(Medicine med : this.medicineCatalog){
+            if(med.getMedicineName().equals(name)){
+                this.medicineCatalog.remove(med);
+                break;
+            }
+        }
+    
+    }
+}
---
+++ LabExerciseThree/src/Model/Observation.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author tanujkodali
+ */
+public class Observation {
+    int observationId;
+    private double bloodPressure;
+    private double temperature;
+    
+    public Observation(){
+        
+        
+    }
+
+    public int getObservationId() {
+        return observationId;
+    }
+
+    public void setObservationId(int observationId) {
+        this.observationId = observationId;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+    
+    @Override
+    
+    public String toString(){
+        return String.valueOf(this.observationId);
+    }
+    
+}
